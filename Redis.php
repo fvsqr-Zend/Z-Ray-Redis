248701ffd51d8f63e02fbeb564f415fa8456ff18
janatzend <jan@zend.com>
2014-11-24 15:48:11 +0100

    initial commit

---
+++ Redis.php
@@ -0,0 +1,87 @@
+<?php
+namespace Redis;
+
+class Redis
+{
+    /**
+     * @var \Redis
+     */
+    private $redis;
+
+    public function init($host, $port)
+    {
+        $this->redis = new \Redis();
+        $this->redis->connect($host, $port);
+    }
+
+    function statusInfo($context, &$storage)
+    {
+        $config = $this->redis->config("GET", "*");
+        $infoServer = $this->redis->info("server");
+        $infoMemory = $this->redis->info("memory");
+
+        $redisConfig = array_map(function ($key) use($config)
+        {
+            return array(
+                'property' => $key,
+                'value' => $config[$key]
+            );
+        }, array_keys($config));
+
+        $redisServerInfo = array_map(function ($key) use($infoServer)
+        {
+            return array(
+                'property' => $key,
+                'value' => $infoServer[$key]
+            );
+        }, array_keys($infoServer));
+
+        $redisMemoryInfo = array_map(function ($key) use($infoMemory)
+        {
+            return array(
+                'property' => $key,
+                'value' => $infoMemory[$key]
+            );
+        }, array_keys($infoMemory));
+
+        $storage['config'] = $redisConfig;
+        $storage['serverInfo'] = $redisServerInfo;
+        $storage['memoryInfo'] = $redisMemoryInfo;
+    }
+
+    function hashGetAll($context, &$storage)
+    {
+        $cacheKey = $context['functionArgs'][0];
+        $value = $this->redis->hGetAll($cacheKey);
+        $ttl = $this->redis->ttl($cacheKey);
+
+        $item = array(
+            'Cache Key' => $cacheKey,
+            'value' => $value,
+            'TTL' => gmdate('H ', $ttl) . 'hours,' . gmdate(' i ', $ttl) . 'minutes, ' . gmdate(' s ', $ttl) . 'seconds',
+            'Item fetch Duration' => $context['durationExclusive'] . ' ms',
+            'Called from File' => $context['calledFromFile'],
+            'Called from Line' => $context['calledFromLine']
+        );
+
+        $storage['cachedItems'][$cacheKey] = $item;
+    }
+    
+    function hSet($context, &$storage) {
+        $cacheKey = $context['functionArgs'][0];
+        $value = $context['functionArgs'][1];
+        
+        if (!is_array($value) && isset($context['functionArgs'][2])) {
+            $value = array($value => $context['functionArgs'][2]);
+        }
+        
+        $item = array(
+            'Cache Key' => $cacheKey,
+            'value' => $value,
+            'Called from File' => $context['calledFromFile'],
+            'Called from Line' => $context['calledFromLine']
+        );
+        
+        $storage['cacheWrite'][$cacheKey] = $item;
+    }
+}
---
+++ zray.php
@@ -0,0 +1,28 @@
+<?php
+namespace Redis;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Redis.php';
+
+$zre = new \ZRayExtension('redis');
+$zrayRedis = new Redis();
+
+$zre->setMetadata(array(
+    'logo' => __DIR__ . DIRECTORY_SEPARATOR . 'redis-logo.png'
+));
+
+$zre->setEnabledAfter('Redis::connect');
+
+$zre->traceFunction(
+    'Redis::connect', 
+    function($context, &$storage) use ($zrayRedis) {
+        $zrayRedis->init(
+            $context['functionArgs'][0], 
+            $context['functionArgs'][1]
+        );
+    },  
+    array($zrayRedis, 'statusInfo')
+);
+
+$zre->traceFunction('Redis::hGetAll', function () {}, array($zrayRedis, 'hashGetAll'));
+$zre->traceFunction('Redis::hMset', function () {}, array($zrayRedis, 'hSet'));
+$zre->traceFunction('Redis::hSet', function () {}, array($zrayRedis, 'hSet'));
